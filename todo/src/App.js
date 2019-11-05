7759bd4a92db957aba5672eb6e1ffce4abe3b2ae
Kris <kristian.claire.101@gmail.com>
2019-11-05 09:44:30 -0800

    adds to AddsToDo

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,10 +1,17 @@
 import React, { useReducer } from 'react';
+import Title from './components/Todo';
+import AddToDo from './components/AddToDo';
 import './App.css';
+import { Reducer } from './reducers/Reducers';
 
 function App() {
   return (
     <div className="App">
-     
+      <Reducer></Reducer>
+      <Title>To Do List</Title>
+      <AddToDo>
+      
+      </AddToDo>
     </div>
   );
 }
